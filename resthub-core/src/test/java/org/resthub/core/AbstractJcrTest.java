99567d765764f5a44b141aed6fba4d0ad59c13e0
Sébastien Deleuze <seb@deleuze.fr>
2009-11-08 20:54:05 +0100

    Various cleanup Rollback to Jackrabbit 1.4.11 due to Session Exception in unit tests

--- resthub-core/src/test/java/org/resthub/core/AbstractJcrTest.java
+++ resthub-core/src/test/java/org/resthub/core/AbstractJcrTest.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "classpath*:appCtxJbpm.xml" , "classpath:appCtxGlobal.xml", "classpath:appCtxJcr.xml" })
+@ContextConfiguration(locations = { "classpath:appCtxGlobal.xml", "classpath:appCtxJcr.xml" })
 @Transactional
 public abstract class AbstractJcrTest   {
 
