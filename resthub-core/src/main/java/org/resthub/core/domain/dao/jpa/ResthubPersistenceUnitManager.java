d611b1ac3461e150247cc10d1165f94435b64542
Sébastien Deleuze <seb@deleuze.fr>
2010-02-19 15:15:07 +0100

    Enable JPA entities distributed in various RESThub modules

--- resthub-core/src/main/java/org/resthub/core/domain/dao/jpa/ResthubPersistenceUnitManager.java
+++ resthub-core/src/main/java/org/resthub/core/domain/dao/jpa/ResthubPersistenceUnitManager.java
@@ -15,31 +15,41 @@ import org.springframework.core.type.filter.TypeFilter;
 import org.springframework.orm.jpa.persistenceunit.DefaultPersistenceUnitManager;
 import org.springframework.orm.jpa.persistenceunit.MutablePersistenceUnitInfo;
 
-public class ResthubPersistenceUnitManager extends
-		DefaultPersistenceUnitManager {
+public class ResthubPersistenceUnitManager extends DefaultPersistenceUnitManager {
+	
+	protected String packagePatternToScan = null;
+	
+	public String getPackagePatternToScan() {
+		return packagePatternToScan;
+	}
+
+	public void setPackagePatternToScan(String packagePatternToScan) {
+		this.packagePatternToScan = packagePatternToScan;
+	}
 
 	@Override
 	protected void postProcessPersistenceUnitInfo(MutablePersistenceUnitInfo pui) {
 
-		ResourcePatternResolver rpr = new PathMatchingResourcePatternResolver(Thread.currentThread().getContextClassLoader());
-		TypeFilter entityTypeFilter = new AnnotationTypeFilter(Entity.class);
-		MetadataReaderFactory metadataReaderFactory = new SimpleMetadataReaderFactory();
-
-		try {
-			Resource[] resources = rpr
-					.getResources("classpath*:org/resthub/**/domain/model/*.class");
-
-			for (Resource resource : resources) {
-				MetadataReader metadataReader = null;
-
-				metadataReader = metadataReaderFactory.getMetadataReader(resource);
-				if (entityTypeFilter.match(metadataReader, metadataReaderFactory)) {
-					pui.addManagedClassName(metadataReader.getClassMetadata().getClassName());
+		if(this.packagePatternToScan != null) {
+		
+			ResourcePatternResolver rpr = new PathMatchingResourcePatternResolver(Thread.currentThread().getContextClassLoader());
+			TypeFilter entityTypeFilter = new AnnotationTypeFilter(Entity.class);
+			MetadataReaderFactory metadataReaderFactory = new SimpleMetadataReaderFactory();
+	
+			try {
+				Resource[] resources = rpr.getResources(this.packagePatternToScan);
+	
+				for (Resource resource : resources) {
+					MetadataReader metadataReader = null;
+	
+					metadataReader = metadataReaderFactory.getMetadataReader(resource);
+					if (entityTypeFilter.match(metadataReader, metadataReaderFactory)) {
+						pui.addManagedClassName(metadataReader.getClassMetadata().getClassName());
+					}
 				}
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
 
 		super.postProcessPersistenceUnitInfo(pui);
