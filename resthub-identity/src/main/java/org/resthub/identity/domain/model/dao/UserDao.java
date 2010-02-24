1c49b99a79b8496d7e2b9edb4c4de612e2467416
Sébastien Deleuze <seb@deleuze.fr>
2010-02-24 15:23:34 +0100

    Fix user DAO

---
+++ resthub-identity/src/main/java/org/resthub/identity/domain/model/dao/UserDao.java
@@ -0,0 +1,8 @@
+package org.resthub.identity.domain.model.dao;
+
+import org.resthub.core.domain.dao.ResourceDao;
+import org.resthub.identity.domain.model.User;
+
+public interface UserDao extends ResourceDao<User> {
+
+}
