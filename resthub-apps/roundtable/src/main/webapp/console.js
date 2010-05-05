ccc6071055ae4ea7f9c2e2ac5f1bba3dcffca585
Sébastien Deleuze <seb@deleuze.fr>
2010-05-05 22:22:55 +0200

    Use dominoes to load css Replace monitoring console iFrame by a DOM insertion

--- resthub-apps/roundtable/src/main/webapp/console.js
+++ resthub-apps/roundtable/src/main/webapp/console.js
@@ -14,7 +14,7 @@
          * WADL console.
          */
         this.get('#/console/wadl', function(context) {
-            dominoes("lib/ui/jquery.ui.button.js lib/ui/jquery.ui.dialog.js components/wadl/wadl-console.js", function() {
+            dominoes("lib/ui/jquery.ui.button.js lib/ui/jquery.ui.dialog.js components/wadl/wadl-console.js $css(components/wadl/wadl-console.css) ", function() {
                 $("#main").wadlConsole();
             });
         });
@@ -23,7 +23,14 @@
          * Monitoring console.
          */
         this.get('#/console/monitoring', function(context) {
-            $('#main').html("<iframe src='console/monitoring'/>");
+			$.ajax({
+                    url: 'console/monitoring',
+                    dataType: 'html',
+                    success: function(data) {
+                        $('#main').html(data);
+                    }
+                });
+            
         });
         
     });
