4947894da1319de8123f0e7d2590e3a58e6df423
Guillaume Zurbach <guillaume.zurbach@gmail.com>
2010-08-18 15:32:04 +0200

    Javascript stack : better errors management in front controller ajax requests.

--- resthub-js/src/main/webapp/lib/core/resthub.wrappers.js
+++ resthub-js/src/main/webapp/lib/core/resthub.wrappers.js
@@ -6,22 +6,40 @@
 	var old_ajax = $.ajax;
 	var resthubSettings = {
 		error: function(request, status, error) {
-			switch (request.status) {
-				case 403:
-					$.pnotify('Forbidden (' + request.status + ').');
-					break;
-				case 404:
-					$.pnotify('No data found (' + request.status + ').');
-					break;
-				case 500:
-					$.pnotify('An error occurred during Ajax request (' + request.status + ').');
-					if( error != undefined ) {
-						console.log(error);
-					}
-					break;
-				default:
-					$.pnotify('An error occurred during Ajax request (' + request.status + ').');
-					break;
+			/*
+			 * If an error message is specified (other than html), it is displayed.
+			 * Otherwise, a generic message is displayed according to the error code.
+			 */
+			if(request.responseText && request.responseText.substring(0, 6) != '<html>')
+			{
+				$.pnotify ({
+					pnotify_title: 'Error ' + request.status,
+					pnotify_text: request.responseText,
+					pnotify_type: 'error'
+				});
+			}
+			else
+			{
+				if (request.status < 400) {
+					$.pnotify ({
+						pnotify_title: 'Information',
+						pnotify_text: request.statusText
+					});
+				}
+				if(request.status >= 400 && request.status < 500) {
+					$.pnotify ({
+						pnotify_title: 'Client error ' + request.status,
+						pnotify_text: request.statusText,
+						pnotify_type: 'error'
+					});
+				}
+				if(request.status >= 500 && request.status < 600) {
+					$.pnotify ({
+						pnotify_title: 'Server error ' + request.status,
+						pnotify_text: request.statusText,
+						pnotify_type: 'error'
+					});
+				}
 			}
 		}
 	};
