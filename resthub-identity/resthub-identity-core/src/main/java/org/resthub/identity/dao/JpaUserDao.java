0d1322985acaa55483770be3591815981ec92dda
MaxenceDalmais <devnull@localhost>
2010-11-16 18:05:37 +0100

    Find user by a query during authentication Merge UserService and authenticationService

--- resthub-identity/resthub-identity-core/src/main/java/org/resthub/identity/dao/JpaUserDao.java
+++ resthub-identity/resthub-identity-core/src/main/java/org/resthub/identity/dao/JpaUserDao.java
@@ -1,8 +1,7 @@
 package org.resthub.identity.dao;
 
-import java.util.List;
-
 import javax.inject.Named;
+import javax.persistence.Query;
 
 import org.resthub.core.dao.GenericJpaResourceDao;
 import org.resthub.identity.model.User;
@@ -12,14 +11,16 @@ public class JpaUserDao extends GenericJpaResourceDao<User> implements UserDao {
 
 	@Override
 	public User getUserByAuthenticationInformation(String login, String password) {
-		List<User> users = this.findEquals("login", login);
-		User returnUser=null;
-		for(User u :users){
-			if(u.getPassword().equals(password)){
-				returnUser=u;
-			break;}
-		}
-		return returnUser;
-		
+
+		Query query = this
+				.getEntityManager()
+				.createQuery(
+						"Select u from User u where u.login= ?1 and u.password =?2");
+		query.setParameter(1, login);
+		query.setParameter(2, password);
+
+		User u=null;
+		try{ u= (User)query.getSingleResult();}catch(Exception e){e.printStackTrace();/*Nothing to do*/}
+		return u;
 	}
 }
--- resthub-identity/resthub-identity-core/src/main/java/org/resthub/identity/service/UserServiceImpl.java
+++ resthub-identity/resthub-identity-core/src/main/java/org/resthub/identity/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package org.resthub.identity.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -8,11 +9,13 @@ import javax.inject.Named;
 import org.resthub.core.annotation.Auditable;
 import org.resthub.core.service.GenericResourceServiceImpl;
 import org.resthub.identity.dao.UserDao;
+import org.resthub.identity.model.Group;
 import org.resthub.identity.model.User;
+import org.resthub.oauth2.provider.service.AuthenticationService;
 import org.springframework.util.Assert;
 
 @Named("userService")
-public class UserServiceImpl extends GenericResourceServiceImpl<User, UserDao> implements UserService {
+public class UserServiceImpl extends GenericResourceServiceImpl<User, UserDao> implements UserService, AuthenticationService{
 
 	@Inject
 	@Named("userDao")
@@ -41,5 +44,20 @@ public class UserServiceImpl extends GenericResourceServiceImpl<User, UserDao> i
 		return dao.getUserByAuthenticationInformation(login,password);		
 	}
 
+	@Override
+	public String getUser(String userName, String password) {
+		User u = this.authenticateUser(userName, password);
+		return (u!=null)? u.getLogin() : null;
+		}
+
+	@Override
+	public List<String> getUserPermissions(String userId) {
+		List<String> userPermissisons = new ArrayList<String>();
+		userPermissisons.addAll(this.findByLogin(userId).getPermissions());
+		for( Group g : this.findByLogin(userId).getGroups()){
+			userPermissisons.addAll(g.getPermissions());
+		}
+		return userPermissisons;
+	}
 
 }
