4b1affc7e5670f4ded61c228433cb06d2e0181a5
Baptiste Meurant <baptiste.meurant@gmail.com>
2010-11-16 01:25:36 +0100

    add javadoc for resthub Tapestry5 spring security module

--- resthub-tapestry5/resthub-tapestry5-spring-security/src/main/java/org/resthub/tapestry5/security/services/LoginService.java
+++ resthub-tapestry5/resthub-tapestry5-spring-security/src/main/java/org/resthub/tapestry5/security/services/LoginService.java
@@ -4,8 +4,23 @@ import java.util.List;
 
 import org.springframework.security.core.GrantedAuthority;
 
+/**
+ * This interface defines methods allowing to programmatically and
+ * dynamically be able to log a user from its login and password
+ * 
+ * @author bmeurant <Baptiste Meurant>
+ */
 public interface LoginService {
 
+	/**
+	 * Try to log the user from username, password and authorities
+	 * 
+	 * @param i_username
+	 * @param i_password
+	 * @param i_authorities
+	 * 
+	 * @return true if user successfully loggued, false otherwise
+	 */
 	public abstract Boolean executeLogin(String i_username, String i_password,
 			List<GrantedAuthority> i_authorities);
 
