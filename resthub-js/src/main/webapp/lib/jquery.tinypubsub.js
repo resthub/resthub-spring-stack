dffeaece92165acf4a91b3dfd483f74d0499f3ae
Sébastien Deleuze <seb@deleuze.fr>
2011-01-18 18:52:47 +0100

    Add tinypubsub, storage in order to prepare Sammy removal

---
+++ resthub-js/src/main/webapp/lib/jquery.tinypubsub.js
@@ -0,0 +1,27 @@
+/*!
+* jQuery Tiny Pub/Sub - v0.3 - 11/4/2010
+* http://benalman.com/
+*
+* Copyright (c) 2010 "Cowboy" Ben Alman
+* Dual licensed under the MIT and GPL licenses.
+* http://benalman.com/about/license/
+*/
+define(['jquery'], function (jQuery) {
+	(function($){
+	  
+	  var o = $({});
+	  
+	  $.subscribe = function() {
+	    o.bind.apply( o, arguments );
+	  };
+	  
+	  $.unsubscribe = function() {
+	    o.unbind.apply( o, arguments );
+	  };
+	  
+	  $.publish = function() {
+	    o.trigger.apply( o, arguments );
+	  };
+	  
+	})(jQuery);
+});
