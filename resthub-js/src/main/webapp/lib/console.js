ba8eb0b1541514ab769a203faf21bf3344d09af3
Florian Traverse <florian.traverse@gmail.com>
2011-01-19 15:35:14 +0100

    added an initial CommonJS minimal browser implementation, not tested.

---
+++ resthub-js/src/main/webapp/lib/console.js
@@ -0,0 +1,31 @@
+/**
+ * Minimal CommonJS console implementation.
+ * Doesn't support advanced feature if not available through original console
+ * @see http://wiki.commonjs.org/wiki/Console
+ * @author Florian Traverse
+ */
+define(function() {
+	var logger,
+		notSupported = function() {};
+	
+	logger = (console && "log" in console) ? console : {log: notSupported };
+	
+	logger.debug = console.debug || logger.log;
+	logger.info = console.info || logger.log;
+	logger.warn = console.warn || logger.log;
+	logger.error = console.error || logger.log;
+
+	//unsupported features
+	logger.trace = console.trace || notSupported;
+	logger.assert = console.assert || notSupported;
+	logger.dir = console.dir || notSupported;
+	logger.dirxml = console.dirxml || notSupported;
+	logger.group = console.group || notSupported;
+	logger.groupEnd = console.groupEnd || notSupported;
+	logger.time = console.time|| notSupported;
+	logger.timeEnd = console.timeEnd|| notSupported;
+	logger.profile = console.profile|| notSupported;
+	logger.profileEnd = console.profileEnd|| notSupported;
+	logger.count = console.count || notSupported;
+	return logger;
+});
--- resthub-js/src/main/webapp/lib/resthub.controller.js
+++ resthub-js/src/main/webapp/lib/resthub.controller.js
@@ -2,38 +2,8 @@
  * Resthub-controller is a generic javascript controller for resthub
  * applications. It provides utility functions for basic op�rations.
  */
-define(['jquery','jqueryui/widget'], function($) {
+define(['jquery', 'jquery.class', 'jqueryui/widget'], function($, Class) {
 	$.widget("ui.controller", {
-		
-		_post: function _post(url, callback, data) {
-			this._ajax(url, callback, 'POST', data);
-		},
-
-		_get: function _get(url, callback) {
-			this._ajax(url, callback, 'GET', null);
-		},
-
-		_put: function _put(url, callback, data) {
-			this._ajax(url, callback, 'PUT', data);
-		},
-
-		_delete: function _delete(url, callback) {
-			this._ajax(url, callback, 'DELETE', null);
-		},
-
-		/**
-		 * Perform basic ajax request and call your widget back.
-		 */
-		_ajax: function(url, callback, type, data) {
-			$.ajax({
-				url: url,
-				dataType: this.options.dataType,
-				contentType: this.options.contentType,
-				type: type,
-				data: data,
-				success: $.proxy( callback , this )
-			});
-		},
 		
 		/**
 		 * Render current widget with the template specified in
@@ -61,7 +31,6 @@ define(['jquery','jqueryui/widget'], function($) {
 		template: function() {
 			return this.options.template;
 		},
-
 		
 		options: {
 			dataType: 'json',
