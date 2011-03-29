bd38590013632a155df39ad835a7d9ddc2eeee02
Nicolas Morel <marsup@gmail.com>
2011-03-29 15:11:43 +0200

    Create a new method to get users from a group

--- resthub-identity/resthub-identity-core/src/main/java/org/resthub/identity/dao/JpaUserDao.java
+++ resthub-identity/resthub-identity-core/src/main/java/org/resthub/identity/dao/JpaUserDao.java
@@ -1,9 +1,12 @@
 package org.resthub.identity.dao;
 
+import java.util.List;
 import javax.inject.Named;
+import javax.persistence.Query;
 
 import org.resthub.core.dao.GenericJpaResourceDao;
 import org.resthub.identity.model.User;
+import org.resthub.identity.model.Group;
 
 /**
  * The JpaUserDao class is based on both {@link GenericJpaResourceDao} and
@@ -12,6 +15,17 @@ import org.resthub.identity.model.User;
  * It is a bean whose name is userDao
  * */
 @Named("userDao")
-public class JpaUserDao extends GenericJpaResourceDao<User> implements PermissionsOwnerDao<User> {
+public class JpaUserDao extends GenericJpaResourceDao<User> implements UserDao {
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<User> getUsersFromGroup(String groupName) {
+        String queryString = "select u from " + this.getDomainClass().getName() + " u JOIN "
+                + Group.class.getName() + " g where g.name=:groupName";
+        Query q = this.getEntityManager().createQuery(queryString);
+        q.setParameter("groupName", groupName);
+        return q.getResultList();
+    }
 }
--- resthub-identity/resthub-identity-core/src/main/java/org/resthub/identity/service/GroupServiceImpl.java
+++ resthub-identity/resthub-identity-core/src/main/java/org/resthub/identity/service/GroupServiceImpl.java
@@ -7,6 +7,7 @@ import javax.inject.Named;
 
 import org.resthub.core.service.GenericResourceServiceImpl;
 import org.resthub.identity.dao.PermissionsOwnerDao;
+import org.resthub.identity.dao.UserDao;
 import org.resthub.identity.model.Group;
 import org.resthub.identity.model.User;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,158 +21,156 @@ import org.springframework.util.Assert;
  * It's a bean whose name is "groupService"
  * */
 @Named("groupService")
-public class GroupServiceImpl extends
-		GenericResourceServiceImpl<Group, PermissionsOwnerDao<Group>> implements
-		GroupService {
+public class GroupServiceImpl extends GenericResourceServiceImpl<Group, PermissionsOwnerDao<Group>> implements
+        GroupService {
 
-	/**
-	 * The userDao<br/>
-	 * This class need it in order to be able to deal with users
-	 */
-	@Inject
-	@Named("userDao")
-	PermissionsOwnerDao<User> userDao;
+    /**
+     * The userDao<br/>
+     * This class need it in order to be able to deal with users
+     */
+    @Inject
+    @Named("userDao")
+    UserDao userDao;
 
-	@Inject
-	@Named("groupDao")
-	public void setResourceDao(PermissionsOwnerDao<Group> groupDao) {
-		super.setDao(groupDao);
-	}
+    @Inject
+    @Named("groupDao")
+    public void setResourceDao(PermissionsOwnerDao<Group> groupDao) {
+        super.setDao(groupDao);
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public Group findByName(String name) {
-		Assert.notNull(name, "Group name can't be null");
-		List<Group> result = this.dao.findEquals("name", name);
-		int size = result.size();
-		Group g = null;
-		if (size == 1) {
-			g = result.get(0);
-		}
-		return g;
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Group findByName(String name) {
+        Assert.notNull(name, "Group name can't be null");
+        List<Group> result = this.dao.findEquals("name", name);
+        int size = result.size();
+        Group g = null;
+        if (size == 1) {
+            g = result.get(0);
+        }
+        return g;
 
-	}
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public List<Group> findAllGroups() {
-		return this.dao.readAll();
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<Group> findAllGroups() {
+        return this.dao.readAll();
+    }
 
-	/**
-	 * add a group to a group
-	 * 
-	 * @param groupName
-	 *            the name of the group to whom the subGroup should be added
-	 * @param subGroupName
-	 *            the name of the group which should be added
-	 * 
-	 */
-	@Override
-	@Transactional
-	public void addGroupToGroup(String groupName, String subGroupName) {
-		if (groupName != null && subGroupName != null) {
-			Group group = this.findByName(groupName);
-			Group subGroup = this.findByName(subGroupName);
-			if (group != null && subGroup != null) {
-				boolean contain = group.getGroups().contains(subGroup);
-				if (!contain) {
-					group.getGroups().add(subGroup);
-				}
-			}
-		}
-	}
+    /**
+     * add a group to a group
+     *
+     * @param groupName
+     *            the name of the group to whom the subGroup should be added
+     * @param subGroupName
+     *            the name of the group which should be added
+     *
+     */
+    @Override
+    @Transactional
+    public void addGroupToGroup(String groupName, String subGroupName) {
+        if (groupName != null && subGroupName != null) {
+            Group group = this.findByName(groupName);
+            Group subGroup = this.findByName(subGroupName);
+            if (group != null && subGroup != null) {
+                boolean contain = group.getGroups().contains(subGroup);
+                if (!contain) {
+                    group.getGroups().add(subGroup);
+                }
+            }
+        }
+    }
 
-	/**
-	 * add a permission to a group
-	 * 
-	 * @param groupName
-	 *            the name of the group
-	 * @param permission
-	 *            the permission to be added
-	 */
-	@Override
-	@Transactional
-	public void addPermissionToGroup(String groupName, String permission) {
-		if (groupName != null && permission != null) {
-			Group g = this.findByName(groupName);
-			if (g != null) {
-				boolean contains = g.getPermissions().contains(permission);
-				if (!contains) {
-					g.getPermissions().add(permission);
-				}
-			}
-		}
-	}
+    /**
+     * add a permission to a group
+     *
+     * @param groupName
+     *            the name of the group
+     * @param permission
+     *            the permission to be added
+     */
+    @Override
+    @Transactional
+    public void addPermissionToGroup(String groupName, String permission) {
+        if (groupName != null && permission != null) {
+            Group g = this.findByName(groupName);
+            if (g != null) {
+                boolean contains = g.getPermissions().contains(permission);
+                if (!contains) {
+                    g.getPermissions().add(permission);
+                }
+            }
+        }
+    }
 
-	/**
-	 * Get the groups direct Permissions
-	 * 
-	 * @param the
-	 *            name of the group to whom the permisisons are requested
-	 * 
-	 * @return the list of permissions that the group has directly (non
-	 *         Inherited)
-	 * */
-	@Override
-	@Transactional
-	public List<String> getGroupDirectPermissions(String groupName) {
-		List<String> permissions = null;
-		if (groupName != null) {
-			Group g = this.findByName(groupName);
-			if (g != null) {
-				permissions = g.getPermissions();
-			}
-		}
-		return permissions;
-	}
+    /**
+     * Get the groups direct Permissions
+     *
+     * @param the
+     *            name of the group to whom the permisisons are requested
+     *
+     * @return the list of permissions that the group has directly (non
+     *         Inherited)
+     * */
+    @Override
+    @Transactional
+    public List<String> getGroupDirectPermissions(String groupName) {
+        List<String> permissions = null;
+        if (groupName != null) {
+            Group g = this.findByName(groupName);
+            if (g != null) {
+                permissions = g.getPermissions();
+            }
+        }
+        return permissions;
+    }
 
-	/**
-	 * remove a group from a group
-	 * 
-	 * @param groupName
-	 *            the name of the group to which the subGroup should be remove
-	 * 
-	 * @param subGroupName
-	 *            the name of the subGroup which should be removed from the
-	 *            other group list
-	 * 
-	 */
-	@Override
-	public void removeGroupFromGroup(String groupName, String subGroupName) {
-		Group g = this.findByName(groupName);
-		if (g != null && subGroupName != null) {
-			List<String> groups = g.getPermissions();
-			while (groups.contains(subGroupName)) {
-				groups.remove(subGroupName);
-			}
-		}
-	}
+    /**
+     * remove a group from a group
+     *
+     * @param groupName
+     *            the name of the group to which the subGroup should be remove
+     *
+     * @param subGroupName
+     *            the name of the subGroup which should be removed from the
+     *            other group list
+     *
+     */
+    @Override
+    public void removeGroupFromGroup(String groupName, String subGroupName) {
+        Group g = this.findByName(groupName);
+        if (g != null && subGroupName != null) {
+            List<String> groups = g.getPermissions();
+            while (groups.contains(subGroupName)) {
+                groups.remove(subGroupName);
+            }
+        }
+    }
 
-	/**
-	 * Remove a permission from a group
-	 * 
-	 * @param groupName
-	 *            the name of the group from which the permission should be
-	 *            removed
-	 * 
-	 * @param permission
-	 *            the permission which should be removed
-	 * */
-	@Override
-	@Transactional
-	public void removePermissionFromGroup(String groupName, String permission) {
-		Group g = this.findByName(groupName);
-		if (g != null && permission != null) {
-			List<String> permissions = g.getPermissions();
-			while (permissions.contains(permission)) {
-				permissions.remove(permission);
-			}
-		}
-	}
-
+    /**
+     * Remove a permission from a group
+     *
+     * @param groupName
+     *            the name of the group from which the permission should be
+     *            removed
+     *
+     * @param permission
+     *            the permission which should be removed
+     * */
+    @Override
+    @Transactional
+    public void removePermissionFromGroup(String groupName, String permission) {
+        Group g = this.findByName(groupName);
+        if (g != null && permission != null) {
+            List<String> permissions = g.getPermissions();
+            while (permissions.contains(permission)) {
+                permissions.remove(permission);
+            }
+        }
+    }
 }
