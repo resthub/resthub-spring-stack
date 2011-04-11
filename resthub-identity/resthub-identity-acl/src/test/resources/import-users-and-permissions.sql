5d6c823cbdfe971d34a2547498d0cc92a4e84abd
Nicolas Morel <marsup@gmail.com>
2011-04-11 17:14:32 +0200

    Fix sql import in identity ACL after seb's modifications

--- resthub-identity/resthub-identity-acl/src/test/resources/import-users-and-permissions.sql
+++ resthub-identity/resthub-identity-acl/src/test/resources/import-users-and-permissions.sql
@@ -1,10 +1,10 @@
 /* login : test , password :  t3st */
 insert into permissions_owner (id) values (1);
-insert into idm_users (id, firstName, lastName, email, login, password) values (1, 'test', 'ing', 'test@resthub.org', 'test', 'lvxWgeRR8QOakoGHnYAdxmW1ujQaauua');
+insert into idm_users (id, firstName, lastName, email, login, password) values (1, 'test', 'ing', 'test@resthub.org', 'test', '{SHA}u3/Rg4+2cdohm4CmQtP9Qq45HX0=');
 insert into permissions_owner_permissions (id, permissions) values (1, 'IM-USER');
 insert into permissions_owner_permissions (id, permissions) values (1, 'CREATE');
 
 /* login : admin , password : 4dm|n */
 insert into permissions_owner (id) values (2);
-insert into idm_users (id, firstName, lastName, email, login, password) values (2, 'alex', 'synclar', 'user1@resthub.org', 'admin', 'nwCxqV005XMHxW4oPNWmZE5MNXqH31sJ');
+insert into idm_users (id, firstName, lastName, email, login, password) values (2, 'alex', 'synclar', 'user1@resthub.org', 'admin', '{SHA}gf3KgPW/QIbAMON8QvYID/ep8AY=');
 insert into permissions_owner_permissions (id, permissions) values (2, 'IM-ADMIN');
