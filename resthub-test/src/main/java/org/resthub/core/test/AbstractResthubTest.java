5bb2e64fb52c4fa9516aceabfc6276ef265429a2
Johann Vanackere <johann.vanackere@gmail.com>
2011-04-14 17:08:30 +0200

    Add myself as developper Add resthub-test module

---
+++ resthub-test/src/main/java/org/resthub/core/test/AbstractResthubTest.java
@@ -0,0 +1,19 @@
+package org.resthub.core.test;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Base class for your unit tests, preconfigure Spring test framework
+ * Ususally you will have to redefine @ContextConfiguration to specify your application context file in addition to resthub ones  
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath*:resthubContext.xml", "classpath*:applicationContext.xml"})
+@TransactionConfiguration(defaultRollback = true)
+@Transactional(readOnly = false)
+public abstract class AbstractResthubTest {
+
+}
