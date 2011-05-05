205aa178400e7e5a2cac68ac5b1255a25dcad179
Baptiste Meurant <baptiste.meurant@gmail.com>
2011-05-05 13:47:23 +0200

    uncomment last commented tests in resthub-core

--- resthub-core/src/main/java/org/resthub/core/test/dao/AbstractDaoTest.java
+++ resthub-core/src/main/java/org/resthub/core/test/dao/AbstractDaoTest.java
@@ -19,113 +19,109 @@ import org.resthub.core.test.AbstractResthubTransactionalTest;
 import org.resthub.core.util.ClassUtils;
 import org.resthub.core.util.MetamodelUtils;
 
-public abstract class AbstractDaoTest<T, PK extends Serializable, D extends GenericDao<T, PK>> extends AbstractResthubTransactionalTest {
+public abstract class AbstractDaoTest<T, PK extends Serializable, D extends GenericDao<T, PK>>
+		extends AbstractResthubTransactionalTest {
 
-    // -----------------------------------------------------------------------------------------------------------------
-    // Attributes
-    /**
-     * The tested DAO
-     */
-    protected D dao;
-    /**
-     * Id of the tested POJO
-     */
-    protected PK id;
-    
-    @PersistenceContext
-    private EntityManager em;
+	/**
+	 * The tested DAO
+	 */
+	protected D dao;
+	/**
+	 * Id of the tested POJO
+	 */
+	protected PK id;
 
-    // -----------------------------------------------------------------------------------------------------------------
-    // Property
-    /**
-     * Injection of DAO.
-     */
-    public void setDao(D dao) {
-        this.dao = dao;
-    } // setDao().
+	@PersistenceContext
+	private EntityManager em;
 
-    // -----------------------------------------------------------------------------------------------------------------
-    // Protected methods
-    /**
-     * Automatically retrieve ID from entity instance.
-     * 
-     * @param obj The object from whom we need primary key
-     * @return The corresponding primary key.
-     */
-    protected PK getIdFromEntity(T obj) {
-        MetamodelUtils utils = new MetamodelUtils<T, PK>((Class<T>) ClassUtils.getGenericTypeFromBean(this.dao), em.getMetamodel());
-        return (PK) utils.getIdFromEntity(obj);
-    }
+	/**
+	 * Injection of DAO.
+	 */
+	public void setDao(D dao) {
+		this.dao = dao;
+	}
 
-    @SuppressWarnings("unchecked")
-    protected T createTestRessource() throws Exception {
-        return (T) ClassUtils.getGenericTypeFromBean(this.dao).newInstance();
-    }
+	/**
+	 * Automatically retrieve ID from entity instance.
+	 * 
+	 * @param obj
+	 *            The object from whom we need primary key
+	 * @return The corresponding primary key.
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	protected PK getIdFromEntity(T obj) {
+		MetamodelUtils utils = new MetamodelUtils<T, PK>(
+				(Class<T>) ClassUtils.getGenericTypeFromBean(this.dao),
+				em.getMetamodel());
+		return (PK) utils.getIdFromEntity(obj);
+	}
 
-    // -----------------------------------------------------------------------------------------------------------------
-    // Setup/finalize
-    @Before
-    public void setUp() throws Exception {
-        T resource = this.createTestRessource();
-        resource = dao.save(resource);
-        this.id = getIdFromEntity(resource);
-    }
-    
-    @After
-    public void tearDown() throws Exception {
-        // Don't use deleteAll because it does not acheive cascade delete
-    	for (T resource : dao.readAll()) {
-        	dao.delete(resource);
-        }
-    }
+	@SuppressWarnings("unchecked")
+	protected T createTestRessource() throws Exception {
+		return (T) ClassUtils.getGenericTypeFromBean(this.dao).newInstance();
+	}
 
-    // -----------------------------------------------------------------------------------------------------------------
-    // Tests methods
-    @Test
-    public abstract void testUpdate() throws Exception;
+	@Before
+	public void setUp() throws Exception {
+		T resource = this.createTestRessource();
+		resource = dao.save(resource);
+		this.id = getIdFromEntity(resource);
+	}
 
-    @Test
-    public void testSave() throws Exception {
-        T resource = this.createTestRessource();
-        resource = dao.save(resource);
+	@After
+	public void tearDown() throws Exception {
+		// Don't use deleteAll because it does not acheive cascade delete
+		for (T resource : dao.readAll()) {
+			dao.delete(resource);
+		}
+	}
 
-        T foundResource = dao.readByPrimaryKey(getIdFromEntity(resource));
-        assertNotNull("Resource not found!", foundResource);
-    }
+	@Test
+	public abstract void testUpdate() throws Exception;
 
-    @Test
-    public void testDelete() throws Exception {
-        T resource = dao.readByPrimaryKey(this.id);
-        dao.delete(resource);
+	@Test
+	public void testSave() throws Exception {
+		T resource = this.createTestRessource();
+		resource = dao.save(resource);
 
-        T foundResource = dao.readByPrimaryKey(this.id);
-        assertNull("Resource not deleted!", foundResource);
-    }
+		T foundResource = dao.readByPrimaryKey(getIdFromEntity(resource));
+		assertNotNull("Resource not found!", foundResource);
+	}
 
-    @Test
-    public void testDeleteById() throws Exception {
-        dao.delete(this.id);
+	@Test
+	public void testDelete() throws Exception {
+		T resource = dao.readByPrimaryKey(this.id);
+		dao.delete(resource);
 
-        T foundResource = dao.readByPrimaryKey(this.id);
-        assertNull("Resource not deleted!", foundResource);
-    }
+		T foundResource = dao.readByPrimaryKey(this.id);
+		assertNull("Resource not deleted!", foundResource);
+	}
 
-    @Test
-    public void testFindAll() throws Exception {
-        List<T> resourceList = dao.readAll();
-        assertTrue("No resources found!", resourceList.size() >= 1);
-    }
+	@Test
+	public void testDeleteById() throws Exception {
+		dao.delete(this.id);
 
-    @Test
-    public void testCount() throws Exception {
-        Long nb = dao.count();
-        assertTrue("No resources found!", nb >= 1);
-    }
+		T foundResource = dao.readByPrimaryKey(this.id);
+		assertNull("Resource not deleted!", foundResource);
+	}
 
-    @Test
-    public void testReadByPrimaryKey() throws Exception {
-        T foundResource = dao.readByPrimaryKey(this.id);
-        assertNotNull("Resource not found!", foundResource);
-        assertEquals("Resource does not contain the correct Id!", this.id, getIdFromEntity(foundResource));
-    }
+	@Test
+	public void testFindAll() throws Exception {
+		List<T> resourceList = dao.readAll();
+		assertTrue("No resources found!", resourceList.size() >= 1);
+	}
+
+	@Test
+	public void testCount() throws Exception {
+		Long nb = dao.count();
+		assertTrue("No resources found!", nb >= 1);
+	}
+
+	@Test
+	public void testReadByPrimaryKey() throws Exception {
+		T foundResource = dao.readByPrimaryKey(this.id);
+		assertNotNull("Resource not found!", foundResource);
+		assertEquals("Resource does not contain the correct Id!", this.id,
+				getIdFromEntity(foundResource));
+	}
 }
--- resthub-core/src/main/java/org/resthub/core/test/service/AbstractServiceTest.java
+++ resthub-core/src/main/java/org/resthub/core/test/service/AbstractServiceTest.java
@@ -2,6 +2,7 @@ package org.resthub.core.test.service;
 
 import java.io.Serializable;
 import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -10,118 +11,114 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.resthub.core.service.GenericService;
-import org.resthub.core.test.AbstractResthubTest;
 import org.resthub.core.test.AbstractResthubTransactionAwareTest;
 import org.resthub.core.util.ClassUtils;
 import org.resthub.core.util.MetamodelUtils;
 
-public abstract class AbstractServiceTest<T, PK extends Serializable, D extends GenericService<T, PK>> extends AbstractResthubTransactionAwareTest {
+public abstract class AbstractServiceTest<T, PK extends Serializable, D extends GenericService<T, PK>>
+		extends AbstractResthubTransactionAwareTest {
 
-    // -----------------------------------------------------------------------------------------------------------------
-    // Attributes
-    /**
-     * The tested Service
-     */
-    protected D service;
-    /**
-     * The tested POJO
-     */
-    protected PK id;
-    
-    @PersistenceContext
-    private EntityManager em;
+	/**
+	 * The tested Service
+	 */
+	protected D service;
+	/**
+	 * The tested POJO
+	 */
+	protected PK id;
 
-    // -----------------------------------------------------------------------------------------------------------------
-    // Property
-    /**
-     * Injection of Service.
-     */
-    public void setService(D service) {
-        this.service = service;
-    }
+	@PersistenceContext
+	private EntityManager em;
 
-    // -----------------------------------------------------------------------------------------------------------------
-    // Protected methods
-    /**
-     * Automatically retrieve ID from entity instance.
-     * 
-     * @param obj The object from whom we need primary key
-     * @return The corresponding primary key.
-     */
-    protected PK getIdFromEntity(T obj) {
-        MetamodelUtils utils = new MetamodelUtils<T, PK>((Class<T>) ClassUtils.getGenericTypeFromBean(this.service), em.getMetamodel());
-        return (PK) utils.getIdFromEntity(obj);
-    }
-    
-    @SuppressWarnings("unchecked")
-    protected T createTestRessource() throws Exception {
-        return (T) ClassUtils.getGenericTypeFromBean(this.service).newInstance();
-    }
+	/**
+	 * Injection of Service.
+	 */
+	public void setService(D service) {
+		this.service = service;
+	}
 
-    // -----------------------------------------------------------------------------------------------------------------
-    // Setup/finalize
-    @Before
-    public void setUp() throws Exception {
-        T resource = service.create(this.createTestRessource());
-        this.id = getIdFromEntity(resource);
-    }
-    
-    @After
-    public void tearDown() throws Exception {
-    	// Don't use deleteAll because it does not acheive cascade delete
-    	for (T resource : service.findAll()) {
-    		service.delete(resource);
-        }
+	/**
+	 * Automatically retrieve ID from entity instance.
+	 * 
+	 * @param obj
+	 *            The object from whom we need primary key
+	 * @return The corresponding primary key.
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	protected PK getIdFromEntity(T obj) {
+		MetamodelUtils utils = new MetamodelUtils<T, PK>(
+				(Class<T>) ClassUtils.getGenericTypeFromBean(this.service),
+				em.getMetamodel());
+		return (PK) utils.getIdFromEntity(obj);
+	}
 
-    }
+	@SuppressWarnings("unchecked")
+	protected T createTestRessource() throws Exception {
+		return (T) ClassUtils.getGenericTypeFromBean(this.service)
+				.newInstance();
+	}
 
-    // -----------------------------------------------------------------------------------------------------------------
-    // Tests methods
-    @Test
-    public void testCreate() throws Exception {
-        T resource = service.create(this.createTestRessource());
+	@Before
+	public void setUp() throws Exception {
+		T resource = service.create(this.createTestRessource());
+		this.id = getIdFromEntity(resource);
+	}
 
-        T foundResource = service.findById(getIdFromEntity(resource));
-        Assert.assertNotNull("Resource not created!", foundResource);
-    }
+	@After
+	public void tearDown() throws Exception {
+		// Don't use deleteAll because it does not acheive cascade delete
+		for (T resource : service.findAll()) {
+			service.delete(resource);
+		}
 
-    @Test
-    public abstract void testUpdate() throws Exception;
+	}
 
-    @Test
-    public void testDelete() throws Exception {
-        T resource = service.findById(this.id);
-        service.delete(resource);
+	@Test
+	public void testCreate() throws Exception {
+		T resource = service.create(this.createTestRessource());
 
-        T foundResource = service.findById(this.id);
-        Assert.assertNull("Resource not deleted!", foundResource);
-    }
+		T foundResource = service.findById(getIdFromEntity(resource));
+		Assert.assertNotNull("Resource not created!", foundResource);
+	}
 
-    @Test
-    public void testDeleteById() throws Exception {
-        T resource = service.findById(this.id);
-        service.delete(getIdFromEntity(resource));
+	@Test
+	public abstract void testUpdate() throws Exception;
 
-        T foundResource = service.findById(this.id);
-        Assert.assertNull("Resource not deleted!", foundResource);
-    }
+	@Test
+	public void testDelete() throws Exception {
+		T resource = service.findById(this.id);
+		service.delete(resource);
 
-    @Test
-    public void testFindById() throws Exception {
-        T resource = service.findById(this.id);
-        Assert.assertNotNull("Resource should not be null!", resource);
-        Assert.assertEquals("Resource id and resourceId should be equals!", this.id, this.getIdFromEntity(resource));
-    }
+		T foundResource = service.findById(this.id);
+		Assert.assertNull("Resource not deleted!", foundResource);
+	}
 
-    @Test
-    public void testFindAll() throws Exception {
-        List<T> resourceList = service.findAll(null).asList();
-        Assert.assertTrue("No resources found!", resourceList.size() >= 1);
-    }
+	@Test
+	public void testDeleteById() throws Exception {
+		T resource = service.findById(this.id);
+		service.delete(getIdFromEntity(resource));
 
-    @Test
-    public void testCount() throws Exception {
-        Long nb = service.count();
-        Assert.assertTrue("No resources found!", nb >= 1);
-    }
+		T foundResource = service.findById(this.id);
+		Assert.assertNull("Resource not deleted!", foundResource);
+	}
+
+	@Test
+	public void testFindById() throws Exception {
+		T resource = service.findById(this.id);
+		Assert.assertNotNull("Resource should not be null!", resource);
+		Assert.assertEquals("Resource id and resourceId should be equals!",
+				this.id, this.getIdFromEntity(resource));
+	}
+
+	@Test
+	public void testFindAll() throws Exception {
+		List<T> resourceList = service.findAll(null).asList();
+		Assert.assertTrue("No resources found!", resourceList.size() >= 1);
+	}
+
+	@Test
+	public void testCount() throws Exception {
+		Long nb = service.count();
+		Assert.assertTrue("No resources found!", nb >= 1);
+	}
 }
