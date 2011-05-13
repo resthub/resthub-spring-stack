2e416fa5bdf42e4586557ba1d89c40bfc047c7e3
Sébastien Deleuze <seb@deleuze.fr>
2011-05-13 13:57:31 +0200

    Update to 1.1-rc6 release version

--- resthub-identity/resthub-identity-core/src/main/java/org/resthub/identity/tools/ApacheSHAPasswordEncryptor.java
+++ resthub-identity/resthub-identity-core/src/main/java/org/resthub/identity/tools/ApacheSHAPasswordEncryptor.java
@@ -1,22 +1,25 @@
 package org.resthub.identity.tools;
 
+import java.security.NoSuchAlgorithmException;
+
 import javax.inject.Named;
-import org.jasypt.digest.StandardStringDigester;
+
 import org.jasypt.util.password.PasswordEncryptor;
+import org.resthub.core.context.persistence.ScanningPersistenceUnitManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- * This class is based on Jasypt and allows you to encrypt a password doing
- * a 1k times SHA-1 and a random salt of 8 bytes by default.
+ * This class is based on native JDK encryption caabilities and allows you to 
+ * encrypt a password doing a non salt SHA-1 encryption.
  *
  * @author "Nicolas Morel <nicolas.morel@atosorigin.com>"
+ * @author "Sebastien Deleuze <sebastien.deleuze@atosorigin.com>"
  */
 @Named("passwordEncryptor")
 public class ApacheSHAPasswordEncryptor implements PasswordEncryptor {
-
-    /**
-     * The internal digester used
-     */
-    private final StandardStringDigester digester;
+	
+	private static final Logger logger = LoggerFactory.getLogger(ApacheSHAPasswordEncryptor.class);
 
     /**
      * <p>
@@ -24,27 +27,23 @@ public class ApacheSHAPasswordEncryptor implements PasswordEncryptor {
      * </p>
      */
     public ApacheSHAPasswordEncryptor() {
-        super();
-
-        this.digester = new StandardStringDigester();
-        this.digester.setAlgorithm("SHA-1");
-        this.digester.setIterations(1);
-        this.digester.setSaltSizeBytes(8);
-        this.digester.setPrefix("{SHA}");
-        this.digester.setInvertPositionOfSaltInMessageBeforeDigesting(true);
-        this.digester.setInvertPositionOfPlainSaltInEncryptionResults(true);
-        this.digester.setUseLenientSaltSizeCheck(true);
-
-        // Initialize the password encryptor with a 32-bit salt (4 bytes)
-        this.digester.setSaltSizeBytes(4);
+        
     }
 
     /**
      * ${@inheritDoc}
      */
     @Override
+    @SuppressWarnings("restriction")
     public String encryptPassword(final String password) {
-        final String encryptedPassword = this.digester.digest(password);
+    	
+		String encryptedPassword = "";
+		try {
+			encryptedPassword = "{SHA}" + new sun.misc.BASE64Encoder().encode(java.security.MessageDigest.getInstance("SHA1").digest(password.getBytes()));
+		} catch (NoSuchAlgorithmException e) {
+			logger.error(e.toString());
+		}
+    	
         return encryptedPassword;
     }
 
@@ -53,7 +52,7 @@ public class ApacheSHAPasswordEncryptor implements PasswordEncryptor {
      */
     @Override
     public boolean checkPassword(final String plainPassword, final String encryptedPassword) {
-        final boolean isValid = this.digester.matches(plainPassword, encryptedPassword);
+        final boolean isValid = encryptPassword(plainPassword).equals(encryptedPassword);
         return isValid;
     }
 }
