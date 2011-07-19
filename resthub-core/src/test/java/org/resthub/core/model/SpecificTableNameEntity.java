e8d49e1a76603ae77d2827a9505854be7939aa44
jvillanti <julien.villanti@gmail.com>
2011-07-19 23:07:53 +0200

    Cleaning

--- resthub-core/src/test/java/org/resthub/core/model/SpecificTableNameEntity.java
+++ resthub-core/src/test/java/org/resthub/core/model/SpecificTableNameEntity.java
@@ -6,53 +6,51 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 /**
- * This is a test entity to validate that we are able to manipulate non
- * extending resource entities named differently than their classes with generic
- * daos & services
+ * This is a test entity to validate that we are able to manipulate resource
+ * entities named differently than their classes with repositories & services
  */
 @Entity(name = "DifferentTableNameEntity")
 @Table(name = "DifferentTableNameEntity")
 public class SpecificTableNameEntity {
 
-    private Long id;
-    private String name;
+	private Long id;
+	private String name;
 
-    /**
-     * Get the id.
-     * 
-     * @return id
-     */
-    @Id
-    @GeneratedValue
-    public Long getId() {
-        return id;
-    }
+	/**
+	 * Get the id
+	 * 
+	 * @return id
+	 */
+	@Id
+	@GeneratedValue
+	public Long getId() {
+		return id;
+	}
 
-    /**
-     * Set the id.
-     * 
-     * @param id
-     *            id
-     */
-    public void setId(Long id) {
-        this.id = id;
-    }
+	/**
+	 * Set the id
+	 * 
+	 * @param id
+	 *            id
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    /**
-     * @return the entity name
-     */
-    public String getName() {
-        return name;
-    }
+	/**
+	 * @return the entity name
+	 */
+	public String getName() {
+		return name;
+	}
 
-    /**
-     * Set the entity name
-     * 
-     * @param name
-     *            name
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
-
+	/**
+	 * Set the entity name
+	 * 
+	 * @param name
+	 *            name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
 }
--- resthub-core/src/test/java/org/resthub/core/model/StandaloneEntity.java
+++ resthub-core/src/test/java/org/resthub/core/model/StandaloneEntity.java
@@ -5,51 +5,50 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 /**
- * This is a test entity to validate that we are able to manipulate non
- * extending resource entities with generic daos & services
+ * This is a test entity to validate that we are able to manipulate resource
+ * entities with repositories & services
  */
 @Entity
 public class StandaloneEntity {
 
-    private Long id;
-    private String name;
+	private Long id;
+	private String name;
 
-    /**
-     * Get the id.
-     * 
-     * @return id
-     */
-    @Id
-    @GeneratedValue
-    public Long getId() {
-        return id;
-    }
+	/**
+	 * Get the id
+	 * 
+	 * @return id
+	 */
+	@Id
+	@GeneratedValue
+	public Long getId() {
+		return id;
+	}
 
-    /**
-     * Set the id.
-     * 
-     * @param id
-     *            id
-     */
-    public void setId(Long id) {
-        this.id = id;
-    }
+	/**
+	 * Set the id
+	 * 
+	 * @param id
+	 *            id
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    /**
-     * @return the entity name
-     */
-    public String getName() {
-        return name;
-    }
+	/**
+	 * @return the entity name
+	 */
+	public String getName() {
+		return name;
+	}
 
-    /**
-     * Set the entity name
-     * 
-     * @param name
-     *            name
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
-
+	/**
+	 * Set the entity name
+	 * 
+	 * @param name
+	 *            name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
 }
