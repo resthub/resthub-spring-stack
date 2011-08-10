c75e61c3dae9ffd93f9f1d9785cd84175326f32d
Julien Villanti <julien.villanti@gmail.com>
2011-08-10 08:54:58 +0200

    Creation of repository for tests in resthub-web-server

---
+++ resthub-web/resthub-web-server/src/test/java/org/resthub/web/repository/WebSampleResourceRepository.java
@@ -0,0 +1,8 @@
+package org.resthub.web.repository;
+
+import org.resthub.web.model.WebSampleResource;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WebSampleResourceRepository extends JpaRepository<WebSampleResource, Long> {
+
+}
