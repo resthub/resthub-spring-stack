7cf00ddd8052a0dc0aac36e2b9472987c7285cef
jvillanti <julien.villanti@gmail.com>
2012-01-25 14:12:15 +0100

    Refactoring Update (Consistency checking of entity id)

--- resthub-web/resthub-web-server/src/main/java/org/resthub/web/controller/GenericControllerImpl.java
+++ resthub-web/resthub-web-server/src/main/java/org/resthub/web/controller/GenericControllerImpl.java
@@ -3,12 +3,7 @@ package org.resthub.web.controller;
 import java.io.Serializable;
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 import org.resthub.core.service.GenericService;
-import org.resthub.core.util.ClassUtils;
-import org.resthub.core.util.MetamodelUtils;
 import org.resthub.web.exception.BadRequestException;
 import org.resthub.web.exception.NotFoundException;
 import org.resthub.web.response.PageResponse;
@@ -28,106 +23,98 @@ public abstract class GenericControllerImpl<T, ID extends Serializable, S extend
 
     protected S service;
 
-    private EntityManager em;
-    
-    @PersistenceContext
-    public void setEntityManager(EntityManager em) {
-        this.em = em;
-    }
-
-    public GenericControllerImpl() {
-
-    }
-
     public void setService(S service) {
         this.service = service;
     }
 
-    public S getService() {
-        return this.service;
-    }
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    @RequestMapping(method = RequestMethod.POST) @ResponseStatus(HttpStatus.CREATED) @ResponseBody
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseBody
     public T create(@RequestBody T entity) {
         return this.service.create(entity);
     }
 
-    @SuppressWarnings({ "rawtypes", "unchecked" })
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    @RequestMapping(value = "{id}", method = RequestMethod.PUT) @ResponseStatus(HttpStatus.OK)
-    public T update(@PathVariable( "id" ) ID id, @RequestBody T entity) {
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    @ResponseStatus(HttpStatus.OK)
+    public T update(@PathVariable("id") ID id, @RequestBody T entity) {
         Assert.notNull(id, "id cannot be null");
         T retreivedEntity = this.service.findById(id);
-        
-        if( retreivedEntity == null ){
-			throw new NotFoundException();
-		}
-        
-        MetamodelUtils utils = new MetamodelUtils<T, ID>((Class<T>) ClassUtils.getGenericTypeFromBean(this.service),
-                em.getMetamodel());
-        Serializable entityId = utils.getIdFromEntity(entity);
-        
-        if ((entityId != null) && !id.equals(this.getIdFromEntity(retreivedEntity))) {
+
+        if (retreivedEntity == null) {
+            throw new NotFoundException();
+        }
+
+        Serializable entityId = this.service.getIdFromEntity(entity);
+        if ((entityId == null) || (!id.equals(entityId))) {
             throw new BadRequestException();
         }
-        if (null == entityId) {
-            utils.setIdForEntity(entity, id);
-        }
+
         return this.service.update(entity);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    @RequestMapping(value = "all", method = RequestMethod.GET) @ResponseBody
+    @RequestMapping(value = "all", method = RequestMethod.GET)
+    @ResponseBody
     public List<T> findAll() {
         return this.service.findAll();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    @RequestMapping(method = RequestMethod.GET) @ResponseBody
-    public PageResponse<T> findAll(@RequestParam( "page" ) Integer page, @RequestParam( "size" ) Integer size) {
-    	
-    	page = (page == null) ? 0 : page;
-    	size = (size == null) ? 5 : size;
-    	
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
+    public PageResponse<T> findAll(@RequestParam("page") Integer page, @RequestParam("size") Integer size) {
+        page = (page == null) ? 0 : page;
+        size = (size == null) ? 5 : size;
+
         return new PageResponse<T>(this.service.findAll(new PageRequest(page, size)));
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    @RequestMapping(value = "{id}",method = RequestMethod.GET ) @ResponseBody
+    @RequestMapping(value = "{id}", method = RequestMethod.GET)
+    @ResponseBody
     public T findById(@PathVariable("id") ID id) {
         T entity = this.service.findById(id);
         if (entity == null) {
-        	throw new NotFoundException();
+            throw new NotFoundException();
         }
 
         return entity;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    @RequestMapping(value = "all", method = RequestMethod.DELETE) @ResponseStatus(HttpStatus.NO_CONTENT)
+    @RequestMapping(value = "all", method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete() {
         this.service.deleteAllWithCascade();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    @RequestMapping(value = "{id}", method = RequestMethod.DELETE) @ResponseStatus(HttpStatus.NO_CONTENT)
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable("id") ID id) {
         this.service.delete(id);
     }
-
-    /**
-     * Automatically retrieve ID from entity instance.
-     * 
-     * @param obj
-     *            The object from whom we need primary key
-     * @return The corresponding primary key.
-     */
-    @SuppressWarnings({ "rawtypes", "unchecked" })
-    protected ID getIdFromEntity(T obj) {
-        MetamodelUtils utils = new MetamodelUtils<T, ID>((Class<T>) ClassUtils.getGenericTypeFromBean(this.service),
-                em.getMetamodel());
-        return (ID) utils.getIdFromEntity(obj);
-    }
-
 }
