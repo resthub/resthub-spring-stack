e8e17a56c3f2465e643f9b42e8af4c2ba1d4a2e8
Sébastien Deleuze <seb@deleuze.fr>
2012-05-30 22:48:58 +0200

    Merge pull request 79 from bclozel
    RESThub HTTP Client refactoring

--- resthub-web/resthub-web-client/src/main/java/org/resthub/web/Client.java
+++ resthub-web/resthub-web-client/src/main/java/org/resthub/web/Client.java
@@ -1,81 +1,126 @@
 package org.resthub.web;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.ning.http.client.AsyncHttpClientConfig.Builder;
+import com.ning.http.client.Realm.AuthScheme;
+import com.ning.http.client.Realm.RealmBuilder;
+import com.ning.http.client.*;
+import java.io.Closeable;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.Future;
-
 import org.resthub.web.oauth2.OAuth2RequestFilter;
-
-import com.ning.http.client.AsyncCompletionHandler;
-import com.ning.http.client.AsyncHttpClient;
-import com.ning.http.client.AsyncHttpClientConfig.Builder;
-import com.ning.http.client.FluentStringsMap;
-import com.ning.http.client.Realm;
-import com.ning.http.client.Realm.AuthScheme;
-import com.ning.http.client.Realm.RealmBuilder;
-import com.ning.http.client.RequestBuilderBase;
 
 /**
  * RESThub AsyncHttpClient wrapper inspired from Play Framework 2 one
- * 
- * Sample usage : User user = Client.url("http://localhost:8080/api/user".jsonPost(user).get().jsonDeserialize(User.class);
  *
- **/
-public class Client {
+ * Sample usage : User user =
+ * Client.url("http://localhost:8080/api/user".jsonPost(user).get().jsonDeserialize(User.class);
+ *
+ *
+ */
+public class Client implements Closeable {
+
+    protected AsyncHttpClient client;
+    protected Builder builder;
+    private String username = null;
+    private String password = null;
+    private String clientId = null;
+    private String clientSecret = null;
+    private String accessTokenEndpoint = null;
+    private AuthScheme scheme = null;
+
+    public Client() {
+        this.builder = new Builder();
+    }
+
+    public Client(Builder builder) {
+        this.builder = builder;
+    }
+
+    public Client setProxy(String host, int port) {
+        this.builder.setProxyServer(new ProxyServer(host, port));
+        return this;
+    }
+
+    /**
+     * Sets the authentication header for the current request.
+     *
+     * @param username
+     * @param password
+     * @param scheme authentication scheme
+     */
+    public Client setAuth(String username, String password, AuthScheme scheme) {
+        this.username = username;
+        this.password = password;
+        this.scheme = scheme;
+        return this;
+    }
+
+    /**
+     * Sets the OAuth2 authentication header for the current request.
+     *
+     * @param username
+     * @param password
+     * @param accessTokenEndpoint
+     * @param clientId
+     * @param clientSecret
+     */
+    public Client setOAuth2(String username, String password, String accessTokenEndpoint, String clientId, String clientSecret) {
+        this.username = username;
+        this.password = password;
+        this.accessTokenEndpoint = accessTokenEndpoint;
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
+        builder.addRequestFilter(new OAuth2RequestFilter(accessTokenEndpoint, clientId, clientSecret));
+        return this;
+    }
 
     /**
      * Prepare a new request. You can then construct it by chaining calls.
      *
      * @param url the URL to request
      */
-    public static RequestHolder url(String url) {
+    public RequestHolder url(String url) {
+        if (this.client == null) {
+            this.client = new AsyncHttpClient(builder.build());
+        }
         return new RequestHolder(url);
+    }
+
+    @Override
+    public void close() {
+        if (client != null) {
+            client.close();
+        }
     }
 
     /**
      * Provides the bridge between the wrapper and the underlying ning request
      */
-    public static class Request extends RequestBuilderBase<Request> {
+    public class Request extends RequestBuilderBase<Request> {
 
-        private Builder builder = new Builder();
-        
         public Request(String method) {
             super(Request.class, method, false);
         }
 
         private Request auth(String username, String password, AuthScheme scheme) {
-            this.setRealm((new RealmBuilder())
-                .setScheme(scheme)
-                .setPrincipal(username)
-                .setPassword(password)
-                .setUsePreemptiveAuth(true)
-                .build());
+            this.setRealm((new RealmBuilder()).setScheme(scheme).setPrincipal(username).setPassword(password).setUsePreemptiveAuth(true).build());
             return this;
-        }
-
-        public void setBuilder(Builder builder) {
-            this.builder = builder;
-        }
-        
-         public Builder getBuilder() {
-            return this.builder;
         }
 
         private Future<Response> execute() {
             Future<Response> future = null;
             try {
-                future = new AsyncHttpClient(builder.build()).executeRequest(request, new AsyncCompletionHandler<Response>() {
-                @Override
-                public Response onCompleted(com.ning.http.client.Response response) {
-                    return new Response(response);
-                }
-            });
+                future = client.executeRequest(request, new AsyncCompletionHandler<Response>() {
+
+                    @Override
+                    public Response onCompleted(com.ning.http.client.Response response) {
+                        return new Response(response);
+                    }
+                });
             } catch (IOException ex) {
                 future.cancel(true);
             }
@@ -86,27 +131,19 @@ public class Client {
     /**
      * provides the User facing API for building WS request.
      */
-    public static class RequestHolder {
+    public class RequestHolder {
 
         private final String url;
-        private Map<String, Collection<String>> headers = new HashMap<String, Collection<String>>();
-        private Map<String, Collection<String>> queryParameters = new HashMap<String, Collection<String>>();
-
+        private Map<String, Collection<String>> headers = new HashMap<>();
+        private Map<String, Collection<String>> queryParameters = new HashMap<>();
         private String body = null;
-        private String username = null;
-        private String password = null;
-        private String clientId = null;
-        private String clientSecret = null;
-        private String accessTokenEndpoint = null;
-        private AuthScheme scheme = null;
-        private Builder builder = new Builder();
 
         public RequestHolder(String url) {
             this.url = url;
         }
 
         /**
-         * Sets a header with the given name, this can be called repeatedly 
+         * Sets a header with the given name, this can be called repeatedly
          *
          * @param name
          * @param value
@@ -116,7 +153,7 @@ public class Client {
                 Collection<String> values = headers.get(name);
                 values.add(value);
             } else {
-                List<String> values = new ArrayList<String>();
+                List<String> values = new ArrayList<>();
                 values.add(value);
                 headers.put(name, values);
             }
@@ -124,7 +161,8 @@ public class Client {
         }
 
         /**
-         * Sets a query parameter with the given name,this can be called repeatedly
+         * Sets a query parameter with the given name,this can be called
+         * repeatedly
          *
          * @param name
          * @param value
@@ -134,43 +172,10 @@ public class Client {
                 Collection<String> values = headers.get(name);
                 values.add(value);
             } else {
-                List<String> values = new ArrayList<String>();
+                List<String> values = new ArrayList<>();
                 values.add(value);
                 queryParameters.put(name, values);
             }
-            return this;
-        }
-
-        /**
-         * Sets the authentication header for the current request.
-         *
-         * @param username
-         * @param password
-         * @param scheme authentication scheme
-         */
-        public RequestHolder setAuth(String username, String password, AuthScheme scheme) {
-            this.username = username;
-            this.password = password;
-            this.scheme = scheme;
-            return this;
-        }
-
-        /**
-         * Sets the OAuth2 authentication header for the current request.
-         *
-         * @param username
-         * @param password
-         * @param accessTokenEndpoint
-         * @param clientId
-         * @param clientSecret
-         */
-        public RequestHolder setOAuth2(String username, String password, String accessTokenEndpoint, String clientId, String clientSecret) {
-            this.username = username;
-            this.password = password;
-            this.accessTokenEndpoint = accessTokenEndpoint;
-            this.clientId = clientId;
-            this.clientSecret = clientSecret;
-            builder.addRequestFilter(new OAuth2RequestFilter(accessTokenEndpoint, clientId, clientSecret));
             return this;
         }
 
@@ -185,7 +190,7 @@ public class Client {
             this.setHeader(Http.ACCEPT, Http.JSON);
             return execute("GET");
         }
-        
+
         public Future<Response> getXml() {
             this.setHeader(Http.ACCEPT, Http.XML);
             return execute("GET");
@@ -239,7 +244,7 @@ public class Client {
             this.setHeader(Http.CONTENT_TYPE, Http.JSON);
             return executeString("POST", JsonHelper.serialize(o));
         }
-        
+
         public Future<Response> xmlPost(Object o) {
             this.setHeader(Http.ACCEPT, Http.XML);
             this.setHeader(Http.CONTENT_TYPE, Http.XML);
@@ -296,42 +301,36 @@ public class Client {
 
         private Future<Response> execute(String method) {
             Request req = new Request(method).setUrl(url).setHeaders(headers).setQueryParameters(new FluentStringsMap(queryParameters));
-            if (this.username != null && this.password != null && this.scheme != null)
-                req.auth(this.username, this.password, this.scheme);
-            if(this.accessTokenEndpoint != null && this.clientId != null && this.clientSecret != null && this.username != null && this.password != null) {
-                req.setBuilder(this.builder);
-                req.setRealm(new Realm.RealmBuilder().setPrincipal(this.username).setPassword(this.password).build());
+            if (username != null && password != null && scheme != null) {
+                req.auth(username, password, scheme);
+            }
+            if (accessTokenEndpoint != null && clientId != null && clientSecret != null && username != null && password != null) {
+                req.setRealm(new Realm.RealmBuilder().setPrincipal(username).setPassword(password).build());
             }
             return req.execute();
         }
 
         private Future<Response> executeString(String method, String body) {
-            Request req = new Request(method).setBody(body)
-                                                 .setUrl(url)
-                                                 .setHeaders(headers)
-                                                 .setQueryParameters(new FluentStringsMap(queryParameters));
-            if (this.username != null && this.password != null && this.scheme != null)
-                req.auth(this.username, this.password, this.scheme);
+            Request req = new Request(method).setBody(body).setUrl(url).setHeaders(headers).setQueryParameters(new FluentStringsMap(queryParameters));
+            if (username != null && password != null && scheme != null) {
+                req.auth(username, password, scheme);
+            }
             return req.execute();
         }
 
         private Future<Response> executeIS(String method, InputStream body) {
-            Request req = new Request(method).setBody(body)
-                                                 .setUrl(url)
-                                                 .setHeaders(headers)
-                                                 .setQueryParameters(new FluentStringsMap(queryParameters));
-            if (this.username != null && this.password != null && this.scheme != null)
-                req.auth(this.username, this.password, this.scheme);
+            Request req = new Request(method).setBody(body).setUrl(url).setHeaders(headers).setQueryParameters(new FluentStringsMap(queryParameters));
+            if (username != null && password != null && scheme != null) {
+                req.auth(username, password, scheme);
+            }
             return req.execute();
         }
 
         private Future<Response> executeFile(String method, File body) {
-            Request req = new Request(method).setBody(body)
-                                                 .setUrl(url)
-                                                 .setHeaders(headers)
-                                                 .setQueryParameters(new FluentStringsMap(queryParameters));
-            if (this.username != null && this.password != null && this.scheme != null)
-                req.auth(this.username, this.password, this.scheme);
+            Request req = new Request(method).setBody(body).setUrl(url).setHeaders(headers).setQueryParameters(new FluentStringsMap(queryParameters));
+            if (username != null && password != null && scheme != null) {
+                req.auth(username, password, scheme);
+            }
             return req.execute();
         }
     }
@@ -382,6 +381,17 @@ public class Client {
                 throw new RuntimeException(e);
             }
         }
+        
+        /**
+         * Deserialize a JSON response with Generic type
+         */
+        public <T> T jsonDeserialize(TypeReference valueTypeRef) {
+            try {
+                return (T) JsonHelper.deserialize(ahcResponse.getResponseBody(), valueTypeRef);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
 
         /**
          * Deserialize a XML response
@@ -393,5 +403,16 @@ public class Client {
                 throw new RuntimeException(e);
             }
         }
+        
+        /**
+         * Deserialize a XML response with Generic type
+         */
+        public <T> T xmlDeserialize(TypeReference valueTypeRef) {
+            try {
+                return XmlHelper.deserialize(ahcResponse.getResponseBody(), valueTypeRef);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }        
     }
 }
