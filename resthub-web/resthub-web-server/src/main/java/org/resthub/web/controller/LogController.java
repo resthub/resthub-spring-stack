8d6dc405d0777c93aa7a8f250c5838dc88c1813d
Sébastien Deleuze <seb@deleuze.fr>
2012-11-12 18:14:05 +0100

    Fix client logging and modify Spring profile name (resthub-client-logging)

--- resthub-web/resthub-web-server/src/main/java/org/resthub/web/controller/LogController.java
+++ resthub-web/resthub-web-server/src/main/java/org/resthub/web/controller/LogController.java
@@ -1,5 +1,7 @@
 package org.resthub.web.controller;
 
+import javax.inject.Inject;
+import javax.inject.Named;
 import org.resthub.web.log.Log;
 import org.resthub.web.log.LogStrategy;
 import org.resthub.web.log.Logs;
@@ -12,11 +14,12 @@ import org.springframework.web.bind.annotation.*;
  * Log Controller
  */
 @Controller
-@Profile("resthub-clientLogging")
+@Profile("resthub-client-logging")
 public class LogController {
 
     private LogStrategy logStrategy;
 
+    @Inject @Named("defaultLogStrategy")
     public void setLogStrategy(LogStrategy logStrategy) {
         this.logStrategy = logStrategy;
     }
@@ -26,16 +29,16 @@ public class LogController {
     public void logAction(@RequestBody Log log, @RequestHeader("User-Agent") String userAgent) {
         log.browser = userAgent;
         switch (log.level) {
-            case DEBUG:
+            case debug:
                 logStrategy.logDebug(log);
                 break;
-            case INFO:
+            case info:
                 logStrategy.logInfo(log);
                 break;
-            case WARN:
+            case warn:
                 logStrategy.logWarn(log);
                 break;
-            case ERROR:
+            case error:
                 logStrategy.logError(log);
                 break;
             default:
---
+++ resthub-web/resthub-web-server/src/main/java/org/resthub/web/log/DefaultLogStrategy.java
@@ -0,0 +1,36 @@
+package org.resthub.web.log;
+
+import java.text.SimpleDateFormat;
+import javax.inject.Named;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Profile;
+
+@Named("defaultLogStrategy")
+@Profile("resthub-client-logging")
+public class DefaultLogStrategy implements LogStrategy {
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger("JSLogger");
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
+
+    @Override
+    public void logError(Log log) {
+        LOGGER.error("[" + log.browser + "] " + DATE_FORMAT.format(log.time)  + " " + log.message);
+    }
+
+    @Override
+    public void logWarn(Log log) {
+        LOGGER.warn("[" + log.browser + "] " + DATE_FORMAT.format(log.time) + " " + log.message);
+    }
+
+    @Override
+    public void logInfo(Log log) {
+        LOGGER.info("[" + log.browser + "] " + DATE_FORMAT.format(log.time)  + " " + log.message);
+    }
+
+    @Override
+    public void logDebug(Log log) {
+        LOGGER.debug("[" + log.browser + "] " + DATE_FORMAT.format(log.time)  + " " + log.message);
+    }
+    
+}
