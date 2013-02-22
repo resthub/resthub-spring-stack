1a4960d8aec631aaad1a5ea8f742738489ee4217
Baptiste Meurant <baptiste.meurant@gmail.com>
2013-02-22 12:08:07 +0100

    ignore non supported class level validation constraints. fixes #185

---
+++ resthub-web/resthub-web-server/src/test/java/org/resthub/web/validation/controller/JsonValidationControllerIntegrationTest.java
@@ -0,0 +1,82 @@
+package org.resthub.web.validation.controller;
+
+import org.apache.commons.io.IOUtils;
+import org.fest.assertions.api.Assertions;
+import org.resthub.test.AbstractWebTest;
+import org.resthub.web.Response;
+import org.resthub.web.validation.ModelConstraint;
+import org.resthub.web.validation.model.AModel;
+import org.resthub.web.validation.model.ClassLevelConstraintModel;
+import org.resthub.web.validation.model.InheritedClassLevelConstraintModel;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+
+public class JsonValidationControllerIntegrationTest extends AbstractWebTest {
+
+    public JsonValidationControllerIntegrationTest() {
+        super("resthub-web-server, resthub-validation", 9798);
+        this.startServerOnce = false;
+    }
+
+    @Test
+    public void testInheritanceAndCompositionModelWithLocale() {
+        InputStream in = this.getClass().getClassLoader().getResourceAsStream("validation/aModel.json");
+        StringWriter writer = new StringWriter();
+
+        try {
+            IOUtils.copy(in, writer);
+        } catch (IOException e) {
+            Assertions.fail("cannot read file", e);
+        }
+
+        String jsonFile = writer.toString();
+        Response response = this.request("api/validation/" + AModel.class.getCanonicalName()).setQueryParameter("locale", "en").get();
+
+        Assertions.assertThat(response.getBody()).isEqualTo(jsonFile);
+    }
+
+    @Test
+    public void testInheritanceAndCompositionModelWithoutLocale() {
+        Response response = this.request("api/validation/" + AModel.class.getCanonicalName()).get();
+
+        Assertions.assertThat(response.getBody()).contains("constraints");
+    }
+
+    @Test
+    public void testInheritanceAndCompositionModelWithLocales() {
+
+        InputStream in = this.getClass().getClassLoader().getResourceAsStream("validation/aModel.json");
+        StringWriter writer = new StringWriter();
+
+        try {
+            IOUtils.copy(in, writer);
+        } catch (IOException e) {
+            Assertions.fail("cannot read file", e);
+        }
+
+        String jsonFile = writer.toString();
+
+        Response response = this.request("api/validation/" + AModel.class.getCanonicalName()).setQueryParameter("locale", "en-en").get();
+
+        Assertions.assertThat(response.getBody()).isEqualTo(jsonFile);
+    }
+
+    @Test
+    public void testNonSupportedClassLevelConstraint() {
+        Response response = this.request("api/validation/" + ClassLevelConstraintModel.class.getCanonicalName()).get();
+
+        Assertions.assertThat(response.getBody()).contains("org.resthub.web.validation.model.ClassLevelConstraintModel")
+            .contains("\"constraints\" : { }");
+    }
+
+    @Test
+    public void testNonSupportedInheritedClassLevelConstraint() {
+        Response response = this.request("api/validation/" + InheritedClassLevelConstraintModel.class.getCanonicalName()).get();
+
+        Assertions.assertThat(response.getBody()).contains("org.resthub.web.validation.model.InheritedClassLevelConstraintModel")
+                .contains("\"type\" : \"Size\"");
+    }
+}
---
+++ resthub-web/resthub-web-server/src/test/java/org/resthub/web/validation/model/InheritedClassLevelConstraintModel.java
@@ -0,0 +1,19 @@
+package org.resthub.web.validation.model;
+
+import org.resthub.web.validation.constraints.TestConstraint;
+
+import javax.validation.constraints.Size;
+
+public class InheritedClassLevelConstraintModel extends ClassLevelConstraintModel {
+
+    private String description;
+
+    @Size(min=1, max= 10)
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
