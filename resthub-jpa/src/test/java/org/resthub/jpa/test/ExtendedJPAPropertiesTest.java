101df1437ecdfb170ffa059ffbab85a9cd329660
Baptiste Meurant <baptiste.meurant@gmail.com>
2014-01-22 23:55:52 +0100

    add tests for jpa properties extension functionality

---
+++ resthub-jpa/src/test/java/org/resthub/jpa/test/ExtendedJPAPropertiesTest.java
@@ -0,0 +1,39 @@
+package org.resthub.jpa.test;
+
+import org.fest.assertions.data.MapEntry;
+import org.resthub.test.AbstractTransactionalTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.testng.annotations.Test;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+@ActiveProfiles("resthub-jpa")
+@ContextConfiguration(locations = {"classpath*:resthubContext.xml", "classpath:extendedJPAApplicationContext.xml"})
+public class ExtendedJPAPropertiesTest extends AbstractTransactionalTest {
+
+    @PersistenceUnit
+    private EntityManagerFactory emf;
+
+    @Test
+    public void testExtendingJPAProperties() {
+        assertThat(emf).isNotNull();
+        assertThat(emf.getProperties()).isNotNull().isNotEmpty();
+
+        // not modified property
+        assertThat(emf.getProperties()).contains(MapEntry.entry("hibernate.hbm2ddl.auto", "update"));
+
+        // modified property
+        assertThat(emf.getProperties()).contains(MapEntry.entry("hibernate.format_sql", "false"));
+
+        // added property with default value
+        assertThat(emf.getProperties()).contains(MapEntry.entry("my-new-prop", "my-value"));
+
+        // added property with value in database.properties
+        assertThat(emf.getProperties()).contains(MapEntry.entry("my-new-prop-in-properties", "my-other-value"));
+    }
+
+}
