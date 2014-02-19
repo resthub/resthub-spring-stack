981631627b7806f0e85128a43a7fe268564de28f
Baptiste Meurant <baptiste.meurant@gmail.com>
2014-02-19 03:22:39 +0100

    add test for ResthubExceptionhandler and refactor

--- resthub-web/resthub-web-server/src/main/java/org/resthub/web/ResthubExceptionHandler.java
+++ resthub-web/resthub-web-server/src/main/java/org/resthub/web/ResthubExceptionHandler.java
@@ -28,53 +28,29 @@ public class ResthubExceptionHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<Object> handleCustomException(Exception ex, WebRequest request) {
 
         HttpHeaders headers = new HttpHeaders();
+        HttpStatus status;
 
         if (ex instanceof IllegalArgumentException) {
-            HttpStatus status = HttpStatus.BAD_REQUEST;
-            return handleIllegalArgument((IllegalArgumentException) ex, headers, status, request);
-        }
-        else if (ex instanceof ValidationException) {
-            HttpStatus status = HttpStatus.BAD_REQUEST;
-            return handleValidation((ValidationException) ex, headers, status, request);
-        }
-        else if (ex instanceof NotFoundException) {
-            HttpStatus status = HttpStatus.NOT_FOUND;
-            return handleNotFound((NotFoundException) ex, headers, status, request);
-        }
-        else if (ex instanceof NotImplementedException) {
-            HttpStatus status = HttpStatus.NOT_IMPLEMENTED;
-            return handleNotImplemented((NotImplementedException) ex, headers, status, request);
+            status = HttpStatus.BAD_REQUEST;
+        } else if (ex instanceof ValidationException) {
+            status = HttpStatus.BAD_REQUEST;
+        } else if (ex instanceof NotFoundException) {
+            status = HttpStatus.NOT_FOUND;
+        } else if (ex instanceof NotImplementedException) {
+            status = HttpStatus.NOT_IMPLEMENTED;
         } else {
             logger.warn("Unknown exception type: " + ex.getClass().getName());
-            HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
             return handleExceptionInternal(ex, null, headers, status, request);
         }
+
+        return handleExceptionInternal(ex, buildRestError(ex, status), headers, status, request);
     }
 
-    protected ResponseEntity<Object> handleIllegalArgument(IllegalArgumentException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        RestError.Builder builder = new RestError.Builder();
-        builder.setCode(status.value());
-        builder.setStatus(status.getReasonPhrase());
-        builder.setThrowable(ex);
-        return handleExceptionInternal(ex, builder.build(), headers, status, request);
-    }
-
-    protected ResponseEntity<Object> handleValidation(ValidationException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+    private RestError buildRestError(Exception ex, HttpStatus status) {
         RestError.Builder builder = new RestError.Builder();
         builder.setCode(status.value()).setStatus(status.getReasonPhrase()).setThrowable(ex);
-        return handleExceptionInternal(ex, builder.build(), headers, status, request);
-    }
-
-    protected ResponseEntity<Object> handleNotFound(NotFoundException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        RestError.Builder builder = new RestError.Builder();
-        builder.setCode(status.value()).setStatus(status.getReasonPhrase()).setThrowable(ex);
-        return handleExceptionInternal(ex, builder.build(), headers, status, request);
-    }
-
-    protected ResponseEntity<Object> handleNotImplemented(NotImplementedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        RestError.Builder builder = new RestError.Builder();
-        builder.setCode(status.value()).setStatus(status.getReasonPhrase()).setThrowable(ex);
-        return handleExceptionInternal(ex, builder.build(), headers, status, request);
+        return builder.build();
     }
 
 }
